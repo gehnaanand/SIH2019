87c31859a1e06f1a4d0a6aa3d832d259e8650551
Paras Naren <parasnaren1998@gmail.com>
2019-03-03 02:08:53 +0530

    Add files via upload
    1. Creating the majority count dataset
    
    2. Generate entity sets with their corresponding different annotations

---
+++ majority_vote.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+
+def generate_majority_dict(path):
+    """
+    return all the tags and counts
+    """
+    infile = pd.read_csv(path)
+
+    majority = {}
+    for i, row in infile.iterrows():
+        j = 2
+        while row[j] != '\r\n' and row[j] != '':
+            if row[j] in majority:
+                majority[row[j]] += 1
+            else:
+                majority[row[j]] = 1
+                
+            j+=1
+    majority_df = pd.DataFrame.from_dict(majority, orient='index')
+    majority_df.to_csv('majority_file.csv')
+    majority = pd.read_csv('majority_file.csv')
+    majority.columns = ['words','counts']
+    return majority
+
+
+def load_entity_sets(m):
+    """
+    load the multi word entities and their 
+    different annotations
+    """
+    entity = {}
+    for i in range(len(m)):
+        tag, count = m.at[i, 'words'], m.at[i, 'counts']
+        text = tag.split('$')
+        query = ""
+        for k in range(len(text)):
+            if k%2 == 0:
+                query += text[k] + ' '
+        
+        query = query.replace(' ', '_').rstrip('_')
+        
+        if query in entity:
+            if tag in entity[query]:
+                continue
+            else:            
+                entity[query][tag] = count
+        else:
+            entity[query] = {tag: count}
+            
+    #entity = pd.DataFrame(entity)
+    return entity
+        
+                
+if __name__ == "__main__":
+    
+    df = []
+    with open('AnnotatedData.tsv', 'r', encoding='utf8') as infile:
+        for row in infile:
+            text = row.split('\t')
+            df.append(text)
+            df = pd.DataFrame(df)
+
+    df = df.apply(lambda x: x.astype(str).str.lower())
+    df = df[df[2] != '\n'].reset_index(drop=True)
+    df = df.apply(lambda x: x.replace('none',''))
+    df.to_csv('annotated_lower_removed.csv', index=False)
+    majority = generate_majority_dict('annotated_lower_removed.csv')
+    entities = load_entity_sets(majority)
+            
+    
+    
